1644a7b22c9df0847edba46cb7975bd54c52c92f
Jon Bash <jonbash@protonmail.com>
2020-06-10 09:44:16 -0700

    finally complete in-place merge sort

--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -27,18 +27,29 @@ def merge_sort(arr):
     return merged
 
 
-# implement an in-place merge sort algorithm
-def merge_in_place(arr, start, mid, end):
-    # Your code here
+# minor irritation; 'in place' implies no return value, but literally editing
+# the array's object in memory, but the tests require a return value -_-
+def merge_sort_in_place(arr: list, left=0, right=None):
+    right = len(arr) - 1 if right is None else right
 
+    if left >= right:  # already sorted; size <= 1
+        return arr
 
-    return arr
+    # merge-sort each half
+    mid1 = (left + right) // 2
+    mid2 = mid1 + 1
+    arr = merge_sort_in_place(arr, left, mid1)
+    arr = merge_sort_in_place(arr, mid2, right)
 
-
-def merge_sort_in_place(arr, l, r):
-    # Your code here
-
-
+    # merge
+    while mid2 <= right:
+        if arr[left] > arr[mid2]:
+            arr.insert(left, arr.pop(mid2))
+            mid2 += 1
+        elif left < mid2 - 1:
+            left += 1
+        else:
+            mid2 += 1
     return arr
 
 
